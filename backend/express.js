1e8c9c9f48608c55ae09e8ce1c38a46233ac76c5
Christabel Gosiora Obi-Nwosu <christabelobi@meta.com>
2025-06-18 12:48:22 -0700

    no added feature - changes made for deployment testing and debuggng

--- backend/express.js
+++ backend/express.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const { PrismaClient } = require('@prisma/client')
 const randomImageUrl = `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/400/300`;
 const prisma = new PrismaClient();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://kudos-board-1-gnuk.onrender.com/' }));
 
 app.get('/boards', async (req, res) => {
   const Boards = await prisma.boards.findMany()
